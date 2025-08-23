fe68c9e63839e1285dad7ee07b88bbc5cb44e3f7
Victor Lopez <vic.m.lopez114@gmail.com>
2025-08-23 10:34:14 -0400

    finished files

---
+++ script.js
@@ -0,0 +1,28 @@
+const exitBtn = document.querySelector('#exit-btn');
+const navModal = document.querySelector('#nav_modal');
+let hamburgerBtn = document.querySelector('#hamburger_menu');
+let modalLinks = document.querySelectorAll('.nav_modal--list');
+
+for (let i = 0; i < modalLinks.length; i++) {
+	modalLinks[i].addEventListener('click', function () {
+		navModal.classList.remove('show_modal');
+	});
+}
+
+hamburgerBtn.addEventListener('click', function () {
+	navModal.classList.add('show_modal');
+});
+
+exitBtn.addEventListener('click', function () {
+	navModal.classList.remove('show_modal');
+});
+
+navBar = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+	if (window.scrollY > 100) {
+		navBar.classList.add('solid');
+	} else {
+		navBar.classList.remove('solid');
+	}
+});
